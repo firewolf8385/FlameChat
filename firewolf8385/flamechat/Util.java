775deaec73d9d60fa7735cd2089cd22cdfe1cdc8
Colby <colbykrawczyk10@gmail.com>
2019-08-24 02:09:50 -0400

    Added Utility Methods
    Utiltiy methods currently include Util#chat, which makes sending a player or command sender a message quicker and easier to read, and Util#log, which makes it easier to log a message to the console.

--- firewolf8385/flamechat/Util.java
+++ firewolf8385/flamechat/Util.java
@@ -1,4 +1,54 @@
 package firewolf8385.flamechat;
 
-public class Util {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Util
+{
+
+    /**
+     * A quicker way to send a CommandSender a message.
+     * @param sender The CommandSender to send a message to.
+     * @param message The message to be sent.
+     */
+    public static void chat(CommandSender sender, String message)
+    {
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+
+    /**
+     * A quicker way to send a player a message.
+     * @param player The player to send a message to.
+     * @param message The message to be sent.
+     */
+    public static void chat(Player player, String message)
+    {
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+
+    /**
+     * Logs a message to the console.
+     * @param type Type of message to log.
+     * @param message The message to log.
+     */
+    public static void log(String type, String message)
+    {
+        switch(type)
+        {
+            case "info":
+                Bukkit.getLogger().info("[FlameChat] " + message);
+                return;
+
+            case "warning":
+                Bukkit.getLogger().warning("[FLameChat] " + message);
+                return;
+
+            case "severe":
+                Bukkit.getLogger().severe("[FlameChat] " + message);
+                return;
+        }
+    }
+
 }
